2d4c8d691817df53270b73ab21131825ccf7bade
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2022-09-13 22:01:36 +0530

    Initial Commit

---
+++ SwiftUI-Property-Wrappers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Property-Wrappers
+//
+//  Created by Sparkout on 13/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Property-Wrappers/SwiftUI_Property_WrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Property_WrappersApp.swift
+//  SwiftUI-Property-Wrappers
+//
+//  Created by Sparkout on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Property_WrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
